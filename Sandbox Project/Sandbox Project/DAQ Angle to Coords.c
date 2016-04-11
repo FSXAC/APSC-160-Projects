0ce630f7e2348578f3cbcbb74535d6861b84fa37
Muchen He <mansurhe@outlook.com>
2016-04-10 18:53:27 -0700

    Very basic voltage output to screen is now working

--- Sandbox Project/Sandbox Project/DAQ Angle to Coords.c
+++ Sandbox Project/Sandbox Project/DAQ Angle to Coords.c
@@ -14,6 +14,13 @@
 // constants
 const int DAQ_CHANNEL = 7;
 
+// sensors
+const int SENSOR_0 = 0;
+const int SENSOR_1 = 1;
+
+// debug
+const int DEBUG = 1;
+
 // function prototypes
 int runArm(void);
 
@@ -26,5 +33,31 @@ int main(void) {
 	}
 	else {
 		return 1;
+	}
+}
+
+// main loop program for the DAQ
+int runArm(void) {
+	double sensor_0_reading;
+	double sensor_1_reading;
+
+	while (continueSuperLoop()) {
+		// read sensor data
+		sensor_0_reading = analogRead(SENSOR_0);
+		sensor_1_reading = analogRead(SENSOR_1);
+
+		// filter out non sense data
+		if (sensor_0_reading < 0) {
+			sensor_0_reading = 0;
+		}
+
+		if (sensor_1_reading < 0) {
+			sensor_1_reading = 0;
+		}
+		
+		// print sensor data to screen
+		if (DEBUG) {
+			printf("S0: %.3f\tS1:%.3f\n", sensor_0_reading, sensor_1_reading);
+		}
 	}
 }
